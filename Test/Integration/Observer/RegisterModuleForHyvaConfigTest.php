025f812d3a661f472fcbc27bb278726a5a5de119
Jisse Reitsma <jisse@yireo.com>
2024-10-04 14:28:54 +0200

    Finalize module tests

--- Test/Integration/Observer/RegisterModuleForHyvaConfigTest.php
+++ Test/Integration/Observer/RegisterModuleForHyvaConfigTest.php
@@ -5,10 +5,12 @@ declare(strict_types=1);
 namespace Yireo\HyvaThemeAutoRegistration\Test\Integration\Observer;
 
 use Magento\Framework\App\ObjectManager;
+use Magento\Framework\Component\ComponentRegistrar;
 use Magento\Framework\DataObject;
 use Magento\Framework\Event\ConfigInterface as EventManagerConfig;
 use Magento\Framework\Event\Observer;
 use Magento\TestFramework\Fixture\AppArea;
+use Magento\TestFramework\Fixture\AppIsolation;
 use Magento\TestFramework\Fixture\ComponentsDir;
 use PHPUnit\Framework\TestCase;
 use Yireo\HyvaThemeAutoRegistration\Observer\RegisterModuleForHyvaConfig;
@@ -21,7 +23,7 @@ class RegisterModuleForHyvaConfigTest extends TestCase
         // @todo: Copy this to integration test helper
         $eventManagerConfig = ObjectManager::getInstance()->get(EventManagerConfig::class);
         $observers = $eventManagerConfig->getObservers('hyva_config_generate_before');
-        
+
         $foundObserver = false;
         $debugInfo = [];
         foreach($observers as $observer) {
@@ -30,23 +32,28 @@ class RegisterModuleForHyvaConfigTest extends TestCase
                 $foundObserver = true;
             }
         }
-        
+
         $this->assertTrue($foundObserver, var_export($debugInfo, true));
     }
-    
-    #[ComponentsDir('./_files')]
+
+    #[AppIsolation(true)]
+    #[ComponentsDir('../../../../vendor/yireo/magento2-hyva-theme-auto-registration/Test/Integration/_modules')]
     public function testIfObserverExecutionIncludesConstructorBasedModules()
     {
-        // @todo: Enable the relevant module
-        
+        $moduleStatus = ObjectManager::getInstance()->get(\Magento\Framework\Module\Status::class);
+        $moduleStatus->setIsEnabled(true, ['Yireo_Test1']);
+
+        $cache = ObjectManager::getInstance()->get(\Magento\Framework\App\Cache::class);
+        $cache->clean();
+
         $observer = $this->getObserver();
         $target = ObjectManager::getInstance()->get(RegisterModuleForHyvaConfig::class);
         $target->execute($observer);
-        
+
         $this->assertNotEmpty($observer->getEvent()->getConfig()['extensions']);
-        $this->assertObserverEventContainsModule($observer, 'Yireo_Foobar1');
+        $this->assertObserverEventContainsModule($observer, 'Yireo_Test1');
     }
-    
+
     private function getObserver(): Observer
     {
         $config = new DataObject(['extensions' => []]);
@@ -55,10 +62,21 @@ class RegisterModuleForHyvaConfigTest extends TestCase
         $observer->setEvent($event);
         return $observer;
     }
-    
+
     private function assertObserverEventContainsModule(Observer $observer, string $moduleName)
     {
+        $componentRegistrar = ObjectManager::getInstance()->get(ComponentRegistrar::class);
+        $modulePath = $componentRegistrar->getPath('module', $moduleName);
+
+        $isModuleFound = false;
         $extensions = $observer->getEvent()->getConfig()['extensions'];
-        $this->assertContains($moduleName, $extensions);
+        foreach($extensions as $extension) {
+            if ($extension['src'] === $modulePath) {
+                $isModuleFound = true;
+            }
+        }
+
+
+        $this->assertTrue($isModuleFound, var_export($extensions, true));
     }
 }
--- Test/Integration/_files/Foo_Bar1/registration.php
+++
@@ -1,6 +0,0 @@
-<?php
-declare(strict_types=1);
-
-use Magento\Framework\Component\ComponentRegistrar;
-
-ComponentRegistrar::register(ComponentRegistrar::MODULE, 'Foo_Bar1', __DIR__);
